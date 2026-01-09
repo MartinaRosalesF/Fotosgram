702c6a51ef3d16c633d74bc43c3b68c3aad449b4
Marti <martina.m.rosales@gmail.com>
2026-01-09 14:33:57 -0300

    implemente mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import * as maplibregl from 'maplibre-gl';
 
 @Component({
   selector: 'app-mapa',
@@ -8,12 +9,63 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  @ViewChild('map') mapElement!: ElementRef;
+
   @Input() coords: string = '';
 
   constructor() { }
 
   ngOnInit() {
     console.log(this.coords)
+
   }
+
+  ngAfterViewInit() {
+
+    const latLng = this.coords.split(',').map(c => Number(c.trim()));
+
+    const lat = latLng[0];
+    const lng = latLng[1];
+
+    if (isNaN(lat) || isNaN(lng)) return;
+
+    const map = new maplibregl.Map({
+      container: this.mapElement.nativeElement,
+      center: [lng, lat],
+      zoom: 13,
+      style: {
+        version: 8,
+        sources: {
+          osm: {
+            type: 'raster',
+            tiles: [
+              'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            ],
+            tileSize: 256
+          }
+        },
+        layers: [
+          { id: 'osm', type: 'raster', source: 'osm' }
+        ]
+      }
+    });
+
+    new maplibregl.Marker()
+      .setLngLat([lng, lat])
+      .addTo(map);
+
+    setTimeout(() => {
+      map.resize();
+    }, 200);
+  }
+
+  ionViewDidEnter() {
+
+
+  }
+
+
 
 }
