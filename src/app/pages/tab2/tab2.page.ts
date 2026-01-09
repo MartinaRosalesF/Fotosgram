05383f231c872a447d416a05f6fd507302d7a73a
Marti <martina.m.rosales@gmail.com>
2026-01-09 11:16:18 -0300

    html crear post

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  tempImages: string[] = [];
 
-  constructor() {}
+  constructor() { }
+
+  crearPost() {
+
+  }
 
 }
